31e508f26da2780b82bb91a88f1e23d40ad81283
yulin <yulin@ruaho.com>
2017-11-24 18:07:24 +0800

    闪屏页

--- src/com/ths/plt/cordova/activity/SplashActivity.java
+++ src/com/ths/plt/cordova/activity/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.ths.plt.cordova.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.Window;
 
@@ -12,11 +14,23 @@ import com.ths.plt.cordova.R;
  */
 public class SplashActivity extends BaseActivity {
 
+    private Handler os = new Handler();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
 
+
+        // TODO: 2017/11/24 延迟进入主界面
+        os.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //进入主页面
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1500);
     }
 }
